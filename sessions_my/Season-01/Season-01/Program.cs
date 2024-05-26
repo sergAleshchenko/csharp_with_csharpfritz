6a5cf39807cb3d9bc4ac420c2ff10aeb0702a961
Sergei Aleshchenko <serg.aleshchenko@gmail.com>
2024-05-26 19:08:58 +0400

    Learning

---
+++ sessions_my/Season-01/Season-01/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+class Test {
+
+    static void display(object o)
+    {
+        Console.WriteLine(o.ToString());
+    }
+
+    static void Main(string[] args)
+    {
+        var i = 10;
+        var someReallyLongVariableName = 9;
+        var foo = "Something";
+
+        display(someReallyLongVariableName);
+
+        var c = 'C';
+        display(c);
+
+        display("Press enter to close...");
+        Console.ReadLine();
+    }
+}
